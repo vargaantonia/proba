2ba03edd911ce1ae2bb8a0d86fae460c01dbc8a1
vargaantonia <145546330+vargaantonia@users.noreply.github.com>
2025-04-07 16:59:08 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -105,11 +105,10 @@ namespace IngatlanokBackend
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseAuthorization();
