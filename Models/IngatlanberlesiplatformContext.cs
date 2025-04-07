b60e186b8a71da4f3ee1fdbf35241ef7a8db423f
vargaantonia <145546330+vargaantonia@users.noreply.github.com>
2025-04-07 17:00:14 +0200

    Update IngatlanberlesiplatformContext.cs

--- Models/IngatlanberlesiplatformContext.cs
+++ Models/IngatlanberlesiplatformContext.cs
@@ -29,9 +29,6 @@ public partial class IngatlanberlesiplatformContext : DbContext
 
     public virtual DbSet<Telepulesek> Telepuleseks { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("SERVER=localhost;PORT=3306;DATABASE=ingatlanberlesiplatform;USER=root;PASSWORD=;SSL MODE=none;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
